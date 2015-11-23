4d63465839fe1d5932d9a9f46f254f901c9bdb04
qcm8866 <quanchimi@foxmail.com>
2015-11-23 14:59:36 +0800

    Numpy数组切片、索引

--- numpy/numpy2/numpy2.py
+++ numpy/numpy2/numpy2.py
@@ -1,3 +1,24 @@
 # -*- coding:utf-8 -*-
 #Numpy索引切片
 __author__ = 'quanchimi'
+
+import numpy as np
+
+arr = np.arange(10)
+print('arr:',arr)
+print('arr[0]:',arr[0],'arr[5]:',arr[5])
+print('arr[5:8]',arr[5:8])
+print(arr)
+print
+#多维数组
+
+arr2d = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr2d)
+print(arr2d[2])
+print(arr2d[0,2])
+print
+
+arr3d =np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr3d)
+print(arr3d[0])
+
