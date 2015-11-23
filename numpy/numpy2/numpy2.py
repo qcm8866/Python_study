de6d4216bc6f97f300383be0aa7509e8064c0949
qcm8866 <quanchimi@foxmail.com>
2015-11-23 21:17:40 +0800

    数组 切片 索引

--- numpy/numpy2/numpy2.py
+++ numpy/numpy2/numpy2.py
@@ -21,4 +21,22 @@ print
 arr3d =np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
 print(arr3d)
 print(arr3d[0])
+print()
 
+#切片索引
+
+print(arr2d)
+print()
+print(arr2d[:2])
+print(arr2d[:2,1:])
+
+print()
+
+#布尔型索引
+
+names = np.array(['Bob','Joe','Will','Bob','Will','Joe','Joe'])
+data =np.random.randn(7,4)
+print('name:',names,names.dtype )
+print(data)
+
+print(data[names=='Bob'])
